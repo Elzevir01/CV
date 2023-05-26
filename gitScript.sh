971d2b439249ea7eb232130a305855eeacefb8ae
elzevir <matiasarques47@gmail.com>
2023-05-25 23:57:26 -0300

    retoques

---
+++ gitScript.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Ruta al archivo "token"
+ruta_token="/home/elzevir/Documentos/token/gtk.txt"
+
+# Leer el archivo "token" y almacenar las líneas en variables
+read -r usuario < "$ruta_token"
+read -r contrasena < "$ruta_token"
+
+# Agrega todos los archivos a git
+git add .
+
+# Commit con el argumento de linea de comando
+git commit -m "$1"
+
+# Ejecutar el comando "git push" con el usuario y la contraseña
+echo "$contrasena" | git push -u "$usuario" master
