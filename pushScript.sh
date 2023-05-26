dc35438dbfee35d9c136381e97cfc3fad215d1e4
elzevir <matiasarques47@gmail.com>
2023-05-26 15:01:13 -0300

    pushScript

--- pushScript.sh
+++ pushScript.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# Ruta al archivo "token"
-ruta_token="/home/elzevir/Documentos/token/gtk.txt"
-
-# Leer el archivo "token" y almacenar las líneas en variables
-read -r usuario < "$ruta_token"
-read -r contrasena < "$ruta_token"
-
 # Agrega todos los archivos a git
 git add .
 
